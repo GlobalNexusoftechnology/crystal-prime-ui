4612ffbaafd04791cd2f9f698b6086f13810eb98
Anil Breadiya <anilbreadiya999@gmail.com>
2025-05-19 18:10:27 +0530

    feat: Add New lead form modal , Add Import excel modal
    - Created the Add new lead modal screen
    - Created Export Excel modal screen
    - Update lead component Component
    - Update Project management

---
+++ src/features/admin/lead-management/Components/new-lead-form/NewLeadForm.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import { InputField } from "@/components";
+import React, { useState } from "react";
+// import { BsArrowLeft } from "react-icons/bs";
+
+type NewLeadFormProps = {
+  setAddLeadModalOpen: (open: boolean) => void;
+};
+
+export function NewLeadForm({ setAddLeadModalOpen }: NewLeadFormProps) {
+  const [step, setStep] = useState(1);
+  const handleCancel = () => {
+    setAddLeadModalOpen(false);
+    setStep(1); // reset step if modal reused
+  };
+
+  const handleNext = () => {
+    // example logic for step advancement
+    if (step === 1) {
+      setStep(2);
+    } else {
+      setAddLeadModalOpen(false);
+    }
+  };
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40 ">
+      {/* <div className="px-3 py-2 bg-slate-200">
+        <p className="flex items-center text-black py-2">
+          <BsArrowLeft
+            className="text-[1rem] 2xl:text-[1vw] mr-2 cursor-pointer"
+            onClick={""}
+          />
+          Back to Leads
+        </p> */}
+      <div className="bg-white rounded-md w-[500px] max-h-[90vh] overflow-y-auto p-6">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-lg font-semibold">Lead Information</h2>
+        </div>
+        <div className="grid grid-cols-1 gap-4 py-2">
+          <InputField
+            label="Full Name"
+            placeholder="Enter Full Name"
+            name="fullName"
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-4 py-2">
+          <InputField
+            label="Email"
+            placeholder="Enter Email"
+            name="email"
+            type="email"
+          />
+          <InputField
+            label="Mobile No"
+            placeholder="Enter Mobile No"
+            name="mobile"
+          />
+        </div>
+        <div className="grid grid-cols-1 gap-4 py-2">
+          <InputField label="Notes" placeholder="Enter Notes" name="notes" />
+        </div>
+
+        <div className="py-2">
+          <label className="block 2xl:text-[1vw] text-gray-700 mb-2 2xl:mb-[0.5vw]">
+            Assigned To
+          </label>
+          <select className="w-full border rounded px-3 py-2 text-sm">
+            <option>Select Staff</option>
+            <option>John Doe</option>
+          </select>
+        </div>
+        <div className="grid grid-cols-2 gap-4 py-2">
+          <InputField
+            label="Nature Of Business"
+            placeholder="Enter Nature Of Business"
+            name="natureOfBusiness"
+          />
+          <InputField
+            label="Business Name"
+            placeholder="Enter Business Name"
+            name="businessName"
+          />
+
+          <InputField
+            label="City Name"
+            placeholder="Enter City Name"
+            name="city"
+          />
+          <InputField label="Status" placeholder="Enter Status" name="status" />
+        </div>
+        <div className="py-2">
+          <label className="block 2xl:text-[1vw] text-gray-700 mb-2 2xl:mb-[0.5vw]">
+            Lead Type
+          </label>
+          <select className="w-full border rounded px-3 py-2 text-sm">
+            <option>Select Lead Type</option>
+          </select>
+        </div>
+
+        <div className="py-2">
+          <label className="block 2xl:text-[1vw] text-gray-700 mb-2 2xl:mb-[0.5vw]">
+            Upload Document
+          </label>
+          <select className="w-full border rounded px-3 py-2 text-sm">
+            <option>Select Document</option>
+          </select>
+        </div>
+
+        <div className="col-span-2 flex items-center space-x-2 py-2">
+          <input type="checkbox" id="upload" />
+          <label htmlFor="upload" className="text-sm">
+            Upload Document
+          </label>
+        </div>
+
+        <div className="flex justify-end mt-6 space-x-4">
+          <button
+            className="border px-4 py-2 rounded text-sm bg-white text-blue-600 hover:bg-gray-100"
+            onClick={handleCancel}
+          >
+            Cancel
+          </button>
+          <button
+            className="bg-blue-600 text-white px-4 py-2 rounded text-sm hover:bg-blue-700"
+            onClick={handleNext}
+          >
+            Add Lead
+          </button>
+        </div>
+      </div>
+    </div>
+    // </div>
+  );
+}
--- src/features/admin/lead-management/LeadManagement.tsx
+++ src/features/admin/lead-management/LeadManagement.tsx
@@ -1,27 +1,52 @@
 "use client";
 
 import { ModalOverlay } from "@/components";
-import { useState, useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import { LeadsListTable } from "../leads-list-table";
 import Image from "next/image";
 import { analyticalCards, ImageRegistry } from "@/constants";
-import { LeadsListTable } from "../leads-list-table";
 import { AnalyticalCard } from "../analytical-card";
+import { ImportExcel, NewLeadForm } from "./Components";
 
 export function LeadManagement() {
   const [isAddLeadModalOpen, setAddLeadModalOpen] = useState(false);
+  const [activeStep, setActiveStep] = useState<"initial" | "addForm" | "excel">(
+    "initial"
+  );
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     console.log("Dropped files:", acceptedFiles);
   }, []);
+
+  const handleOpenForm = (value: string) => {
+    if (value === "addForm") {
+      setActiveStep("addForm");
+      setAddLeadModalOpen(false);
+    } else if (value === "excel") {
+      setActiveStep("excel");
+      
+    } else {
+      setActiveStep("initial");
+      setAddLeadModalOpen(false)
+    }
+
+  };
+
+  const handleAddFormClose = () =>{
+    setActiveStep("initial")
+  }
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
     <section className="flex flex-col gap-6 md:gap-8 2xl:gap-[2.5vw] border border-gray-300 rounded-lg 2xl:rounded-[0.5vw] bg-white p-4 2xl:p-[1vw]">
       <div className="flex flex-col gap-2 2xl:gap-[0.5vw] px-4 2xl:px-[1vw]">
-        <h1 className="text-xl 2xl:text-[1.25vw] font-medium">Lead Management</h1>
+        <h1 className="text-xl 2xl:text-[1.25vw] font-medium">
+          Lead Management
+        </h1>
       </div>
+
       <div className="grid grid-cols-1 gap-4 2xl:gap-[1vw]">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-4 2xl:gap-[1vw] flex-wrap px-4 2xl:px-[1vw]">
           {analyticalCards.map((card, index) => (
@@ -30,91 +55,105 @@ export function LeadManagement() {
         </div>
         <LeadsListTable setAddLeadModalOpen={setAddLeadModalOpen} />
       </div>
+
       <ModalOverlay
         isOpen={isAddLeadModalOpen}
-        onClose={() => setAddLeadModalOpen(false)}
+        onClose={() => handleOpenForm("")}
         modalClassName="w-[45rem] h-[35rem]"
       >
-        <div className="bg-[#F8F8F8]">
-          <div className="space-y-6 p-4 bg-white ">
-            <h3 className="text-sm font-semibold text-start">
-              {" "}
-              - Back to Leads
-            </h3>
-            <div className="border-2 p-4 w-full space-y-4">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 bg-white  rounded-xl">
-                {/* Manual Upload */}
-                <div className="border-2 border-dashed rounded-xl p-4 text-center hover:shadow transition bg-[#F8F8F8]">
-                  <div className=" w-[15rem] h-[6rem] pb-4">
+        <div className="bg-[#F8F8F8] h-[31rem]">
+          <div className="space-y-6 p-4 bg-white">
+            {activeStep === "initial" && (
+              <div className="border-2 p-4 w-full space-y-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 bg-white rounded-xl">
+                  {/* Manual Upload */}
+                  <div
+                    className="border-2 border-dashed rounded-xl p-4 text-center hover:shadow transition bg-[#F8F8F8]"
+                    onClick={() => handleOpenForm("addForm")}
+                  >
+                    <div className="w-[15rem] h-[6rem] pb-4">
+                      <Image
+                        src={ImageRegistry.onelead}
+                        alt="manual"
+                        width={200}
+                        height={120}
+                        className="object-contain w-full h-full"
+                      />
+                    </div>
+                    <input type="file" hidden id="manual-upload" />
+                    <label
+                      htmlFor="manual-upload"
+                      className="text-purple-600 font-medium cursor-pointer"
+                    >
+                      Click to upload
+                    </label>
+                    <p className="text-sm text-gray-500">One Lead at a time</p>
+                  </div>
+
+                  {/* Excel Upload */}
+                  <div
+                    {...getRootProps()}
+                    onClick={() => handleOpenForm("excel")}
+                    className={`border-2 border-dashed rounded-xl p-4 text-center cursor-pointer transition bg-[#F8F8F8] ${
+                      isDragActive ? "bg-purple-100" : "hover:shadow"
+                    }`}
+                  >
+                    <input {...getInputProps()} />
+                    <div className="w-[15rem] h-[6rem] pb-4">
+                      <Image
+                        src={ImageRegistry.excel}
+                        alt="excel"
+                        width={200}
+                        height={120}
+                        className="mx-auto mb-4 object-contain w-full h-full"
+                      />
+                    </div>
+                    <p className="text-purple-600 font-medium">
+                      Click to upload
+                    </p>
+                    <p className="text-sm text-gray-500">or drag and drop</p>
+                    <p className="text-xs text-gray-400 mt-1">
+                      .XLS, .XLSX (max. 5MB)
+                    </p>
+                  </div>
+                </div>
+
+                {/* External Upload */}
+                <div className="md:col-span-2 border-2 border-dashed rounded-xl p-2 text-center bg-[#F8F8F8] flex justify-center items-center flex-col">
+                  <div className="w-[18rem] h-[8rem] pb-4">
                     <Image
-                      src={ImageRegistry.onelead}
-                      alt="manual"
+                      src={ImageRegistry.external}
+                      alt="external"
                       width={200}
                       height={120}
-                      className=" object-contain w-full h-full"
+                      className="object-contain w-full h-full"
                     />
                   </div>
-                  <input type="file" hidden id="manual-upload" />
+                  <input type="file" hidden id="external-upload" />
                   <label
-                    htmlFor="manual-upload"
+                    htmlFor="external-upload"
                     className="text-purple-600 font-medium cursor-pointer"
                   >
                     Click to upload
                   </label>
-                  <p className="text-sm text-gray-500">One Lead at a time</p>
-                </div>
-
-                {/* Excel Upload with Dropzone */}
-                <div
-                  {...getRootProps()}
-                  className={`border-2 border-dashed rounded-xl p-4 text-center cursor-pointer transition bg-[#F8F8F8] ${
-                    isDragActive ? "bg-purple-100" : "hover:shadow"
-                  }`}
-                >
-                  <input {...getInputProps()} />
-                  <div className="w-[15rem] h-[6rem] pb-4">
-                    <Image
-                      src={ImageRegistry.excel}
-                      alt="excel"
-                      width={200}
-                      height={120}
-                      className="mx-auto mb-4 object-contain w-full h-full"
-                    />
-                  </div>
-                  <p className="text-purple-600 font-medium">Click to upload</p>
                   <p className="text-sm text-gray-500">or drag and drop</p>
                   <p className="text-xs text-gray-400 mt-1">
-                    .XLS, .XLSX (max. 5MB)
+                    SVG, PNG, JPG or GIF (max. 800×400px)
                   </p>
                 </div>
               </div>
-              {/* External Upload */}
-              <div className="md:col-span-2 border-2 border-dashed rounded-xl p-2 text-center bg-[#F8F8F8]  flex justify-center items-center flex-col ">
-                <div className="w-[18rem] h-[8rem] pb-4">
-                  <Image
-                    src={ImageRegistry.external}
-                    alt="external"
-                    width={200}
-                    height={120}
-                    className="object-contain w-full h-full"
-                  />
-                </div>
-                <input type="file" hidden id="external-upload" />
-                <label
-                  htmlFor="external-upload"
-                  className="text-purple-600 font-medium cursor-pointer"
-                >
-                  Click to upload
-                </label>
-                <p className="text-sm text-gray-500">or drag and drop</p>
-                <p className="text-xs text-gray-400 mt-1">
-                  SVG, PNG, JPG or GIF (max. 800×400px)
-                </p>
-              </div>
-            </div>
+            )}
+
+            {activeStep === "excel" && (
+              <ImportExcel setAddLeadModalOpen={setAddLeadModalOpen} />
+            )}
           </div>
         </div>
       </ModalOverlay>
+      
+      {activeStep === "addForm" && (
+        <NewLeadForm setAddLeadModalOpen={handleAddFormClose} />
+      )}
     </section>
   );
 }
