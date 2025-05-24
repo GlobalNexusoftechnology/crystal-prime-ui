e71c366182827b50fe625b54f9906cf1edfdc710
aaftab sheikh <aaftab.sheikh@purpleskyinfotech.com>
2025-05-24 11:22:02 +0530

    chore: add logout functionality
    - Added logout functionality

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,15 +1,24 @@
 "use client";
+
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 import { Loading } from "@/components";
+import { useAuthStore } from "@/services";
 
 export default function AdminPage() {
   const router = useRouter();
+  const { activeSession } = useAuthStore()
 
   useEffect(() => {
-    router.replace("/admin/dashboard");
-  }, [router]);
+    if (!activeSession?.access_token) {
+      // If no token, redirect to login
+      router.replace("/login");
+    } else {
+      // If token exists, redirect to dashboard
+      router.replace("/admin/dashboard");
+    }
+  }, [activeSession?.access_token, router]);
 
   return <Loading />;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,10 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/constants/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./utils/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
