20074afe9c3c2aac4f3b5044386a595d6e6d0599
aaftab sheikh <aaftab.sheikh@purpleskyinfotech.com>
2025-05-25 18:38:23 +0530

    chore: add get random color helper function
    - Added get random color helper function.

--- src/components/table/components/table-row/TableRow.tsx
+++ src/components/table/components/table-row/TableRow.tsx
@@ -1,23 +1,23 @@
 import { ITableRowProps } from "@/constants"; // Adjust this path if needed
-import { getStatusBadge } from "@/utils";     // Utility function for status badge color
+import { getInitials, getRandomColor } from "@/utils";
 import { FiMoreVertical } from "react-icons/fi";
 
-export function TableRow<T extends { id: string | number; status?: string }>(
-  {
-    row,
-    columns,
-    actions = [],
-    openActionId,
-    setOpenActionId,
-    index,
-  }: ITableRowProps<T> & {
-    index: number;
-    openActionId: string | number | null;
-    setOpenActionId: (id: string | number | null) => void;
-  }
-) {
+export function TableRow<
+  T extends { id: string | number; assigned_to?: string }
+>({
+  row,
+  columns,
+  actions = [],
+  openActionId,
+  setOpenActionId,
+  index,
+}: ITableRowProps<T> & {
+  index: number;
+  openActionId: string | number | null;
+  setOpenActionId: (id: string | number | null) => void;
+}) {
   const isOpen = openActionId === row.id;
-  const statusClass = row.status ? getStatusBadge(row.status) : "";
+  // const statusClass = row.assigned_to ? getStatusBadge(row.assigned_to) : "";
 
   return (
     <tr className="border-t 2xl:border-[0.1vw] border-gray-200 hover:bg-gray-50 relative">
@@ -25,24 +25,34 @@ export function TableRow<T extends { id: string | number; status?: string }>(
         {String(index + 1).padStart(3, "0")}
       </td>
       {columns.map((col, index) => (
-        <td key={index} className="p-3 2xl:p-[0.75vw] text-[0.9rem] 2xl:text-[0.9vw] 2xl:leading-[1.3vw] text-gray-700">
-          {col.accessor === "status_id" && row.status ? (
-            <span className={`px-3 py-1 text-xs font-semibold rounded-full ${statusClass}`}>
-              {row.status}
-            </span>
+        <td
+          key={index}
+          className="p-3 2xl:p-[0.75vw] text-[0.9rem] 2xl:text-[0.9vw] 2xl:leading-[1.3vw] text-gray-700"
+        >
+          {col.accessor === "assigned_to" && row.assigned_to ? (
+            <div className="flex items-center gap-2">
+              <p
+                className="p-4 2xl:p-[1vw] text-[0.9rem] 2xl:text-[0.9vw] 2xl:leading-[1.3vw] rounded-full"
+                style={{ backgroundColor: getRandomColor(row.assigned_to) }}
+              >
+                {getInitials(row.assigned_to)}
+              </p>
+              <p
+                className={`px-3 py-1 text-[0.9rem] 2xl:text-[0.9vw] 2xl:leading-[1.3vw]`}
+              >
+                {row.assigned_to}
+              </p>
+            </div>
           ) : (
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             (row as any)[col.accessor]
           )}
         </td>
       ))}
-
       {actions.length > 0 && (
         <td className="p-3 2xl:p-[0.75vw] relative">
           <button
-            onClick={() =>
-              setOpenActionId(isOpen ? null : row.id)
-            }
+            onClick={() => setOpenActionId(isOpen ? null : row.id)}
             className="p-1 2xl:p-[0.25vw] rounded hover:bg-gray-200"
           >
             <FiMoreVertical className="w-5 2xl:w-[1.25vw] h-5 2xl:h-[1.25vw]" />
@@ -53,7 +63,9 @@ export function TableRow<T extends { id: string | number; status?: string }>(
               {actions.map((action, actionIndex) => (
                 <button
                   key={actionIndex}
-                  className={`block w-full px-4 text-[0.9rem] 2xl:text-[0.9vw] 2xl:leading-[1.3vw] 2xl:px-[1vw] py-2 2xl:py-[0.5vw] text-left hover:bg-gray-100 ${action.className || ""}`}
+                  className={`block w-full px-4 text-[0.9rem] 2xl:text-[0.9vw] 2xl:leading-[1.3vw] 2xl:px-[1vw] py-2 2xl:py-[0.5vw] text-left hover:bg-gray-100 ${
+                    action.className || ""
+                  }`}
                   onClick={() => {
                     setOpenActionId(null);
                     action.onClick(row);
