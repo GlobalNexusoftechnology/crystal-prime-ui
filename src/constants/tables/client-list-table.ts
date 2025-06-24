d3e6e1ec39985b213a83935d68c71524a1d8b450
Aaftab Sheikh <aaftab.sheikh@purpleskyinfotech.com>
2025-06-24 20:37:55 +0530

    chore: resolve build issue
    Resolved build issue

--- src/constants/tables/client-list-table.ts
+++ src/constants/tables/client-list-table.ts
@@ -1,18 +1,3 @@
-/**
- * Document List Table Configuration
- *
- * This module defines the data structure, dummy data, table column configurations,
- * and available row actions for the Document List feature in the Document Management module.
- *
- * Exports:
- * - IClientListProps: TypeScript interface for individual document metadata.
- * - ClientList: Sample data representing document records.
- * - documentation: Row-level action buttons (e.g., Edit, View, Delete, Export).
- * - ClientListColumn: Column definitions for rendering the document table.
- */
-
-import { ITableColumn } from "../table";
-
 export interface IClientListProps {
   id: string;
   created_at: string;
@@ -26,44 +11,8 @@ export interface IClientListProps {
   website: string;
   company_name: string;
   contact_person: string;
-  lead_id: null
+  lead_id: null;
 }
-
-
-// export const clientListColumn: ITableColumn<IClientListProps>[] = [
-//   {
-//     header: "CUSTOMER NAME",
-//     accessor: "name",
-//   },
-//   {
-//     header: "COMPANY NAME",
-//     accessor: "company_name",
-//   },
-//   {
-//     header: "CONTACT PERSON",
-//     accessor: "contact_person",
-//   },
-//   {
-//     header: "PHONE NUMBER",
-//     accessor: "contact_number",
-//   },
-//   {
-//     header: "CONTACT EMAIl",
-//     accessor: "email",
-//   },
-//   {
-//     header: "WEBSITE URL",
-//     accessor: "website",
-//   },
-//   {
-//     header: "ADDRESS",
-//     accessor: "address",
-//   },
-//   {
-//     header: "CREATED AT",
-//     accessor: "created_at",
-//   },
-// ];
 
 export const clientListColumn = [
   {
