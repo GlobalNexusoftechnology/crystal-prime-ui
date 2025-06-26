5ff3700637dc3f1ce0fefc6d94d570b18efde725
Aaftab Sheikh <aaftab.sheikh@purpleskyinfotech.com>
2025-06-26 14:48:22 +0530

    chore: resolve build issue
    - Resolved build issue

--- src/services/apis/clients/community-client/query-hooks/useAllLeadDownloadExcelQuery.ts
+++ src/services/apis/clients/community-client/query-hooks/useAllLeadDownloadExcelQuery.ts
@@ -1,11 +1,9 @@
-import { useQuery } from '@/services';
 import { COMMUNITY_CLIENT } from '../communityClient';
 import type { LeadsListFilters } from './useAllLeadsListQuery';
 
 /**
  * This is to track the list of leads list from the backend.
  */
-const ALL_LEAD_DOWNLOAD_EXCEL_QUERY_KEY = 'all-lead-download-excel-query-key';
 
 /**
  * This hook fetches a list of all the leads list in the bloom portal.
