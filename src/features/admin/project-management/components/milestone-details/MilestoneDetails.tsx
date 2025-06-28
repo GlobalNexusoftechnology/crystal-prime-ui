fb776069d04d7242d6ca11458d4a5dc15fb52460
Aaftab Sheikh <aaftab.sheikh@purpleskyinfotech.com>
2025-06-28 11:04:29 +0530

    chore: add formatting date function
    - Added formatting date function

--- src/features/admin/project-management/components/milestone-details/MilestoneDetails.tsx
+++ src/features/admin/project-management/components/milestone-details/MilestoneDetails.tsx
@@ -3,7 +3,7 @@
 import { Breadcrumb } from "@/features";
 import { HeaderDetails } from "../header-details";
 import { IProjectMilestoneResponse } from '@/services';
-import { formatDate } from "@/utils";
+import { formattingDate } from "@/utils";
 import { MilestoneEstimate, MilestoneInfo, TaskTabs } from "./components";
 
 export function MilestoneDetails({ milestoneData }: { milestoneData: IProjectMilestoneResponse }) {
@@ -21,14 +21,14 @@ export function MilestoneDetails({ milestoneData }: { milestoneData: IProjectMil
             name: milestoneData.name,
             assigned_to: milestoneData.assigned_to || '',
             description: milestoneData.description,
-            created_at: formatDate(`${milestoneData.created_at}`),
-            updated_at: formatDate(`${milestoneData.updated_at}`),
+            created_at: formattingDate(milestoneData.created_at, "toReadable"),
+            updated_at: formattingDate(milestoneData.updated_at, "toReadable"),
           }} />
         </div>
         <MilestoneEstimate milestoneEstimateData={{
-          start_date: formatDate(`${milestoneData.start_date}`) || '',
-          end_date: formatDate(`${milestoneData.end_date}`) || '',
-          actual_start: milestoneData.actual_date ? formatDate(`${milestoneData.actual_date}`) : '--',
+          start_date: milestoneData.start_date ? formattingDate(milestoneData.start_date, "toReadable") : '--',
+          end_date:  milestoneData.end_date ? formattingDate(milestoneData.end_date, "toReadable") : '--',
+          actual_start: milestoneData.actual_date ? formattingDate(milestoneData.actual_date, "toReadable") : '--',
         }} />
       </div>
       <TaskTabs
