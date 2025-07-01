a0134c8e46db018fa9d59a74125a3bf1facc6b4c
Aaftab Sheikh <aaftab.sheikh@purpleskyinfotech.com>
2025-07-01 17:37:37 +0530

    chore: add focus for the search border
    - Added focus for the search border

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -38,7 +38,7 @@ export function SearchBar({
   const inputValue = value !== undefined ? value : query;
 
   return (
-    <div className={`bg-${bgColor} relative flex items-center border 2xl:border-[0.1vw] border-gray-300 px-4 py-2 2xl:px-[1vw] 2xl:py-[0.5vw] rounded-xl 2xl:rounded-[0.75vw] ${width}`}>
+    <div className={`bg-${bgColor} focus-within:border-primary relative flex items-center border 2xl:border-[0.1vw] border-gray-300 px-4 py-2 2xl:px-[1vw] 2xl:py-[0.5vw] rounded-xl 2xl:rounded-[0.75vw] ${width}`}>
       {/* Search Icon */}
       <span
         className="cursor-pointer"
@@ -57,8 +57,8 @@ export function SearchBar({
         value={inputValue}
         ref={inputRef}
         onChange={(e) => {
-          if (value === undefined) setQuery(e.target.value); // Only update local state if uncontrolled
-          onSearch(e.target.value); // Trigger the search function
+          if (value === undefined) setQuery(e.target.value); 
+          onSearch(e.target.value); 
         }}
         placeholder={placeholder}
         className="ml-2 2xl:ml-[0.5vw] bg-transparent focus:outline-none w-full 2xl:text-[1vw]"
