774fcea436b16cb3f2556ffe417615ec88a0a13e
Faizahmedchoudhary <ahmedfaiz868@gmail.com>
2025-11-22 14:32:31 +0530

    changes

--- src/features/admin/material-management/components/index.ts
+++ src/features/admin/material-management/components/index.ts
@@ -1,6 +1,4 @@
 export { AddMaterialModal } from "./add-material-modal";
 export { MaterialListTable } from "./material-list-table";
-export { BOQ } from "./boq";
-export { BOQList } from "./boq-list";
 export { MaterialFilters } from "./material-filters";
 export { Proposal, EditProposal } from "./proposal";
