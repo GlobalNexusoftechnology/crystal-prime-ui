5acaac59f70307702a18431f649d62c488c95193
Faizahmedchoudhary <ahmedfaiz868@gmail.com>
2025-11-22 14:15:38 +0530

    changes

--- src/features/admin/material-management/components/index.ts
+++ src/features/admin/material-management/components/index.ts
@@ -4,6 +4,3 @@ export { BOQ } from "./boq";
 export { BOQList } from "./boq-list";
 export { MaterialFilters } from "./material-filters";
 export { Proposal, EditProposal } from "./proposal";
-export { SalesOrder } from "./sales-order";
-export { PurchaseOrder } from "./purchase-order";
-export { PurchaseOrderForm } from "./purchase-order/components/PurchaseOrderForm";
