f6886aa18f270e63d6723557551c3885961f45b8
GlobalNexusoftechnology <globalnexusoftechnologyy@gmail.com>
2026-02-12 13:06:00 +0530

    Update FinancialSummary.tsx

--- src/features/admin/reports/components/business-report/components/financial-summary/FinancialSummary.tsx
+++ src/features/admin/reports/components/business-report/components/financial-summary/FinancialSummary.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FiInfo } from "react-icons/fi";
+// import { FiInfo } from "react-icons/fi";
 
 interface FinancialSummaryProps {
   data: {
@@ -16,17 +16,17 @@ interface FinancialSummaryProps {
 }
 
 export const FinancialSummary: React.FC<FinancialSummaryProps> = ({ data }) => {
-  const metrics = [
-    { label: "Total Income", value: data.totalIncome },
-    { label: "Amount Received in UPI", value: data.amountReceivedInUPI },
-    { label: "Amount Received in Bank", value: data.amountReceivedInBank },
-    { label: "Amount Received in Cash", value: data.amountReceivedInCash },
-    { label: "Amount Received in Online", value: data.amountReceivedInOnline },
-    { label: "Amount Spent in UPI", value: data.amountSpentInUPI },
-    { label: "Amount Spent in Bank", value: data.amountSpentInBank },
-    { label: "Amount Spent in Cash", value: data.amountSpentInCash },
-    { label: "Amount Spent in Online", value: data.amountSpentInOnline },
-  ];
+  // const metrics = [
+  //   { label: "Total Income", value: data.totalIncome },
+  //   { label: "Amount Received in UPI", value: data.amountReceivedInUPI },
+  //   { label: "Amount Received in Bank", value: data.amountReceivedInBank },
+  //   { label: "Amount Received in Cash", value: data.amountReceivedInCash },
+  //   { label: "Amount Received in Online", value: data.amountReceivedInOnline },
+  //   { label: "Amount Spent in UPI", value: data.amountSpentInUPI },
+  //   { label: "Amount Spent in Bank", value: data.amountSpentInBank },
+  //   { label: "Amount Spent in Cash", value: data.amountSpentInCash },
+  //   { label: "Amount Spent in Online", value: data.amountSpentInOnline },
+  // ];
 
   return (
     // <div className="border-b border-gray-400  p-6 ">
