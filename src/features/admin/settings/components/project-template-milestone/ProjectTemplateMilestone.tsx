30cd38c0fa89aa518c886a5bac0ed347f5c72be5
Faizahmedchoudhary <ahmedfaiz868@gmail.com>
2026-05-19 22:29:21 +0530

    changes

--- src/features/admin/settings/components/project-template-milestone/ProjectTemplateMilestone.tsx
+++ src/features/admin/settings/components/project-template-milestone/ProjectTemplateMilestone.tsx
@@ -8,31 +8,51 @@ import { MilestoneRow } from "./components";
 import toast from "react-hot-toast";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function ProjectTemplateMilestone({ formik, readOnly = false }: { formik: FormikProps<any>; readOnly?: boolean }) {
+export function ProjectTemplateMilestone({
+  formik,
+  readOnly = false,
+}: {
+  formik: FormikProps<any>;
+  readOnly?: boolean;
+}) {
   const { values, handleChange, handleBlur } = formik;
-  const [openMilestones, setOpenMilestones] = useState<{ [key: string]: boolean }>(() => {
+  const [openMilestones, setOpenMilestones] = useState<{
+    [key: string]: boolean;
+  }>(() => {
     if (values.milestones && values.milestones.length > 0) {
       // Open all milestones by default
       return Object.fromEntries(
-        values?.milestones?.map((m: Milestone, idx: number) => [(m.id || String(idx)), true])
+        values?.milestones?.map((m: Milestone, idx: number) => [
+          m.id || String(idx),
+          true,
+        ]),
       );
     }
     return {};
   });
-  const [editingMilestoneId, setEditingMilestoneId] = useState<string | null>(() => {
-    if (values?.milestones && values.milestones.length === 1 && !values.milestones[0].id) {
-      return "0";
-    }
-    return null;
-  });
+  const [editingMilestoneId, setEditingMilestoneId] = useState<string | null>(
+    () => {
+      if (
+        values?.milestones &&
+        values.milestones.length === 1 &&
+        !values.milestones[0].id
+      ) {
+        return "0";
+      }
+      return null;
+    },
+  );
   const [editingTaskId, setEditingTaskId] = useState<string | null>(null);
 
   useEffect(() => {
     if (values.milestones && values.milestones.length > 0) {
       setOpenMilestones(
         Object.fromEntries(
-          values?.milestones?.map((m: Milestone, idx: number) => [(m.id || String(idx)), true])
-        )
+          values?.milestones?.map((m: Milestone, idx: number) => [
+            m.id || String(idx),
+            true,
+          ]),
+        ),
       );
     }
   }, [values.milestones]);
@@ -49,7 +69,7 @@ export function ProjectTemplateMilestone({ formik, readOnly = false }: { formik:
   const handleCancel = () => {
     setEditingMilestoneId(null);
     setEditingTaskId(null);
-  }
+  };
 
   return (
     <div className="p-4 ">
@@ -71,7 +91,10 @@ export function ProjectTemplateMilestone({ formik, readOnly = false }: { formik:
                       const firstMilestone = milestones[0];
                       if (
                         milestones.length === 0 ||
-                        (firstMilestone && firstMilestone.name && firstMilestone.estimated_days && firstMilestone.description)
+                        (firstMilestone &&
+                          firstMilestone.name &&
+                          firstMilestone.estimated_days &&
+                          firstMilestone.description)
                       ) {
                         const newMilestone = {
                           id: "",
@@ -83,7 +106,9 @@ export function ProjectTemplateMilestone({ formik, readOnly = false }: { formik:
                         arrayHelpers.unshift(newMilestone);
                         setEditingMilestoneId("0");
                       } else {
-                        toast.error("Please complete the current milestone before adding a new one.");
+                        toast.error(
+                          "Please complete the current milestone before adding a new one.",
+                        );
                       }
                     }}
                   >
@@ -96,6 +121,9 @@ export function ProjectTemplateMilestone({ formik, readOnly = false }: { formik:
               </span>
               <span className="text-[0.9rem]  font-medium text-gray-500">
                 Description
+              </span>
+              <span className="text-[0.9rem]  font-medium text-gray-500">
+                Assignee
               </span>
             </div>
             {values.milestones?.map((milestone: Milestone, index: number) => {
@@ -115,9 +143,16 @@ export function ProjectTemplateMilestone({ formik, readOnly = false }: { formik:
                   onSave={() => {
                     setEditingMilestoneId(null);
                     const currentMilestone = values.milestones[index];
-                    const firstTask = currentMilestone && currentMilestone.tasks && currentMilestone.tasks[0];
+                    const firstTask =
+                      currentMilestone &&
+                      currentMilestone.tasks &&
+                      currentMilestone.tasks[0];
                     if (firstTask) {
-                      setEditingTaskId((firstTask.id ? firstTask.id : String(0)) + '-' + Date.now());
+                      setEditingTaskId(
+                        (firstTask.id ? firstTask.id : String(0)) +
+                          "-" +
+                          Date.now(),
+                      );
                     } else {
                       setEditingTaskId(null);
                     }
@@ -137,4 +172,4 @@ export function ProjectTemplateMilestone({ formik, readOnly = false }: { formik:
       />
     </div>
   );
-} 
+}
